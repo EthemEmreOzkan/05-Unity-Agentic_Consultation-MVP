75d8de1d3b6ab302ac419ee142cead3592efa557
Ethem_Emre_Ozkan <ethememreozkan@gmail.com>
2025-11-23 21:58:06 +0300

    main: fix

---
+++ Assets/Scripts/LLM_Scripts/Api_Key_SO.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Api_Key", menuName = "Scriptable Objects/Api_Key")]
+public class Api_Key_SO : ScriptableObject
+{
+    public string Default_Key = "AIzaSyDIlZNJl8EELBOnQony__TBepROtyyCyJ8";
+    public string Player_Key = "";
+}
